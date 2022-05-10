a0239758aa5bcf68b57439577d2afef96e10ff20
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-05-10 11:25:04 -0400

    Add events to system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,19 +3,34 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
+use tauri::Manager;
 
 fn main() {
   let tray_menu = SystemTrayMenu::new()
-    .add_item(CustomMenuItem::new("quit".to_string(), "Quit"))
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(CustomMenuItem::new("hide".to_string(), "Hide"));
+    .add_item(CustomMenuItem::new("hide".to_string(), "Hide"))
+    .add_item(CustomMenuItem::new("quit".to_string(), "Quit"));
 
   let system_tray = SystemTray::new()
     .with_menu(tray_menu);
 
   tauri::Builder::default()
     .system_tray(system_tray)
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::MenuItemClick { id, .. } => {
+        match id.as_str() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "hide" => {
+            let window = app.get_window("main").unwrap();
+            window.hide().unwrap();
+          }
+          _ => {}
+        }
+      }
+      _ => {}
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
