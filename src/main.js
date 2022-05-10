8cd23e9078ebdc2da55e9ebcd710536084251730
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-05-10 14:50:37 -0400

    Format files and add better title bar for macOS

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
-import { appWindow } from '@tauri-apps/api/window'
-import App from './App.svelte'
+import { platform } from '@tauri-apps/api/os';
+import { appWindow } from '@tauri-apps/api/window';
+import App from './App.svelte';
 
-document
-  .getElementById('titlebar-minimize')
-  .addEventListener('click', () => appWindow.minimize())
-document
-  .getElementById('titlebar-maximize')
-  .addEventListener('click', () => appWindow.toggleMaximize())
-document
-  .getElementById('titlebar-close')
-  .addEventListener('click', () => appWindow.close())
+platform().then((platform) => {
+	if (platform !== 'darwin') {
+		document.getElementById('titlebar-buttons').style.setProperty('display', 'flex');
+	}
+});
+
+document.getElementById('titlebar-minimize').addEventListener('click', () => {
+	appWindow.minimize();
+});
+document.getElementById('titlebar-maximize').addEventListener('click', () => {
+	appWindow.toggleMaximize();
+});
+document.getElementById('titlebar-close').addEventListener('click', () => {
+	appWindow.close();
+});
 
 const app = new App({
-  target: document.getElementById('app')
-})
+	target: document.getElementById('app')
+});
 
-export default app
+export default app;
