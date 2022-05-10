935d0a5eaffc17101f86b48770efa2ff08deddd9
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-05-10 10:44:26 -0400

    Add custom titlebar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import { appWindow } from '@tauri-apps/api/window'
 import App from './App.svelte'
+
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
 
 const app = new App({
   target: document.getElementById('app')
