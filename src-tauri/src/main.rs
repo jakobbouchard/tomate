047596fb78809f20d2e03d3c82ba708ffdd3385b
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-05-10 10:21:36 -0400

    Add basic system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,19 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(CustomMenuItem::new("quit".to_string(), "Quit"))
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(CustomMenuItem::new("hide".to_string(), "Hide"));
+
+  let system_tray = SystemTray::new()
+    .with_menu(tray_menu);
+
   tauri::Builder::default()
+    .system_tray(system_tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
