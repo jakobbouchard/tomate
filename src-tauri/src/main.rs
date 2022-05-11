76384a5e45edb146cc147c9ceab082647dfb4f5f
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-05-11 11:12:23 -0400

    Remove custom titlebar for now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,43 +6,6 @@
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
 use tauri::Manager;
 
-use cocoa::appkit::{NSWindow, NSWindowStyleMask};
-use tauri::{Runtime, Window};
-
-pub trait WindowExt {
-  #[cfg(target_os = "macos")]
-  fn set_transparent_titlebar(&self, transparent: bool);
-}
-
-impl<R: Runtime> WindowExt for Window<R> {
-  #[cfg(target_os = "macos")]
-  fn set_transparent_titlebar(&self, transparent: bool) {
-    use cocoa::appkit::NSWindowTitleVisibility;
-
-    unsafe {
-      let id = self.ns_window().unwrap() as cocoa::base::id;
-
-      let mut style_mask = id.styleMask();
-      style_mask.set(
-        NSWindowStyleMask::NSFullSizeContentViewWindowMask,
-        transparent,
-      );
-      id.setStyleMask_(style_mask);
-
-      id.setTitleVisibility_(if transparent {
-        NSWindowTitleVisibility::NSWindowTitleHidden
-      } else {
-        NSWindowTitleVisibility::NSWindowTitleVisible
-      });
-      id.setTitlebarAppearsTransparent_(if transparent {
-        cocoa::base::YES
-      } else {
-        cocoa::base::NO
-      });
-    }
-  }
-}
-
 fn main() {
   let tray_menu = SystemTrayMenu::new()
     .add_item(CustomMenuItem::new("hide".to_string(), "Hide"))
@@ -67,12 +30,6 @@ fn main() {
         }
       }
       _ => {}
-    })
-    .setup(|app| {
-      let win = app.get_window("main").unwrap();
-      win.set_transparent_titlebar(true);
-
-      Ok(())
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
--- src/main.js
+++ src/main.js
@@ -1,22 +1,4 @@
-import { platform } from '@tauri-apps/api/os';
-import { appWindow } from '@tauri-apps/api/window';
 import App from './App.svelte';
-
-platform().then((platform) => {
-	if (platform !== 'darwin') {
-		document.getElementById('titlebar-buttons').style.setProperty('display', 'flex');
-	}
-});
-
-document.getElementById('titlebar-minimize').addEventListener('click', () => {
-	appWindow.minimize();
-});
-document.getElementById('titlebar-maximize').addEventListener('click', () => {
-	appWindow.toggleMaximize();
-});
-document.getElementById('titlebar-close').addEventListener('click', () => {
-	appWindow.close();
-});
 
 const app = new App({
 	target: document.getElementById('app')
