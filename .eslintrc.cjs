7e1f5538c3d4da68b3aab1350a5c333ea682865e
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-11-01 23:53:04 -0400

    Use pnpm, update all deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,13 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:editorconfig/noconflict',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint', 'svelte3', 'editorconfig'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
